22c3e90ee5d5b561041e0c3d4ea0dffa0b47425f
unknown <tejasvibhadrinath26@gmail.com>
2025-06-07 19:57:43 -0700

    only admin can add product

--- ReactApp/src/app/ApplicationComponent/Product/ProductComponent.js
+++ ReactApp/src/app/ApplicationComponent/Product/ProductComponent.js
@@ -11,25 +11,22 @@ let ProductComponent = (props) => {
     let rating = useRef(null)
 
     let product = useSelector((state)=>state.productReducer.Product) //product reducer
-
+    let user = useSelector((state) => state.userReducer.user);
     let dispatchProduct = useDispatch();
 
     //useEffect - reads all the changes that affect the state of the component and then does the job of re-render
     //if we pass the second parameter to initialize the state it will work as - componentDidMount
     //if we dont pass the second parameter to initialize the state it will work as - shouldComponentUpdate
     //if we return a child function then it will work as - componentWillUnMount
-    useEffect(()=>{
-        //initializing the values we read from reducer to our product state
-        name.current.value = product.name
-        price.current.value = product.price
-        desc.current.value = product.desc
-        rating.current.value = product.rating
+useEffect(() => {
+    if (isAdmin && name.current && price.current && desc.current && rating.current) {
+        name.current.value = product.name || "";
+        price.current.value = product.price || "";
+        desc.current.value = product.desc || "";
+        rating.current.value = product.rating || "";
+    }
+}, [isAdmin, product]);
 
-        //componentWillUnmount
-        // return ()=>{
-        //     console.log("This section works as component will unmount to clear all subscriptions")
-        // }
-    }, [])
 
     let saveProductClick = (evt)=>{
         //creating product data model to be saved in db using product api
@@ -46,45 +43,56 @@ let ProductComponent = (props) => {
         evt.preventDefault();
     }
 
+    //check if the user is admin, only then render the product adding part to all othe user just display the products
+     const isAdmin = user && user.userName && user.userName.toLowerCase() === "admin";
+
     return(
         <div className="col-md-12">
             <h1 className="col-md-12">Product component</h1>
 
-            <form className={"form componentClass"}>
-                <div className="form col-md-8">
-                    <div className="col-md-12">
-                        <b>Product Name</b>
-                        <input type="text" className="form-control col-md-6 name" ref={name} maxLength={25} 
-                        placeholder="Product Name" />
-                    </div>
-                    <div className="col-md-12">
-                        <b>Price </b>
-                        <input type="number" className="form-control col-md-6" ref={price} 
-                        placeholder="Product Price" />
-                    </div>
-                    
-                    <div className="col-md-12">
-                        <b>Description </b>
-                    <input type="text" className="form-control col-md-6" ref={desc} 
-                        placeholder="Please Describe the product"  />
-                    </div>
-                    
-                    <div className="col-md-12">
-                        <b>Ratings </b>
-                    <input type="text" className="form-control col-md-6" ref={rating} 
-                        placeholder="Ratings" />
-                    </div>
+            {isAdmin && (
+                <form className={"form componentClass"}>
+                    <div className="form col-md-8">
+                        <div className="col-md-12">
+                            <b>Product Name</b>
+                            <input type="text" className="form-control col-md-6 name" ref={name} maxLength={25} 
+                            placeholder="Product Name" />
+                        </div>
+                        <div className="col-md-12">
+                            <b>Price </b>
+                            <input type="number" className="form-control col-md-6" ref={price} 
+                            placeholder="Product Price" />
+                        </div>
+                        
+                        <div className="col-md-12">
+                            <b>Description </b>
+                        <input type="text" className="form-control col-md-6" ref={desc} 
+                            placeholder="Please Describe the product"  />
+                        </div>
+                        
+                        <div className="col-md-12">
+                            <b>Ratings </b>
+                        <input type="text" className="form-control col-md-6" ref={rating} 
+                            placeholder="Ratings" />
+                        </div>
 
-                    <input type="button" className={"form-control btn btn-primary col-md-3"} 
-                        value={"Save Product"} 
-                        onClick={saveProductClick}/>
+                        <input type="button" className={"form-control btn btn-primary col-md-3"} 
+                            value={"Save Product"} 
+                            onClick={saveProductClick}/>
+                    </div>
+                </form>
+            )}
+
+            {!isAdmin && (
+                <div className="alert alert-info col-md-6">
+                    <strong>Info:</strong> Only admin users can add or edit products.
                 </div>
-            </form>
+            )}
+
             <hr/>
             <DisplayProducts />
         </div>
     )
-
 }
 
 export default ProductComponent;
