5dec435a5ba91ea729902c999fe56bea1a2289d4
unknown <tejasvibhadrinath26@gmail.com>
2025-06-21 13:26:58 -0700

    some cleanup

--- ReactApp/src/app/CommonComponent/HeaderComponent.js
+++ ReactApp/src/app/CommonComponent/HeaderComponent.js
@@ -34,7 +34,7 @@ let Header = (props)=>{
                     <NavLink to="/cart" className="button" activeclassname="true"> Cart </NavLink>
                     <NavLink to="/checkout" className="button" activeclassname="true"> Checkout </NavLink>
                     <NavLink to="/recent-orders" className="button" activeclassname="true"> RecentOrders </NavLink>
-                    <NavLink to="/hook" className="button" activeclassname="true"> Hooks </NavLink>
+                    {/*<NavLink to="/hook" className="button" activeclassname="true"> Hooks </NavLink>*/}
                 </>
             )}
         </div>
